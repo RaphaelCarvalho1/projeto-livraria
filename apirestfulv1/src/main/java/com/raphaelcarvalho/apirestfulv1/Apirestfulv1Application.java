a89340ef84d84b139931e69992195d132696df71
RaphaelCarvalho1 <ra_carvalho@id.uff.br>
2024-10-09 18:33:43 -0300

    Criando projeto Spring Boot e modelagem

---
+++ apirestfulv1/src/main/java/com/raphaelcarvalho/apirestfulv1/Apirestfulv1Application.java
@@ -0,0 +1,18 @@
+package com.raphaelcarvalho.apirestfulv1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestfulv1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestfulv1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
